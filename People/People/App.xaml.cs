a64e04a007db3b3645fa16be9f5f6dd107a8aa23
diishguu <vasilub20@gmail.com>
2026-06-16 19:04:46 +0700

    Save14

---
+++ People/People/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using People.Models;
+
+namespace People;
+
+public partial class App : Application
+{
+	public static PersonRepository PersonRepo { get; private set; }
+
+	public App()
+	{
+        InitializeComponent();
+		string dbPath = Path.Combine(
+			FileSystem.AppDataDirectory,
+			"people.db3");
+
+		PersonRepo = PersonRepo;
+    }
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
