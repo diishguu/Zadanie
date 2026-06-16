9c0a4afe92cd05d0868331741763c8cf638c5c1e
diishguu <vasilub20@gmail.com>
2026-06-16 16:42:45 +0700

    Save9

--- Zadanie/MainPage.xaml.cs
+++ Zadanie/MainPage.xaml.cs
@@ -1,12 +1,36 @@
-﻿using System.Threading.Tasks;
+﻿
+using Microsoft.Maui;
+using System.Threading.Tasks;
 
 namespace Zadanie
 {
     public partial class MainPage : ContentPage
     {
-       public MainPage()
+        private Color colorNavy = Colors.Navy;
+        private Color colorSilver = Colors.Silver;
+
+        public MainPage()
         {
             InitializeComponent();
+        }
+        void OnLight(object sender, EventArgs e)
+        {
+            LayoutRoot.BackgroundColor = colorSilver;
+
+            tipLabel.TextColor = colorNavy;
+          
+            totalLabel.TextColor = colorNavy;
+           
+        }
+
+        void OnDark(object sender, EventArgs e)
+        {
+            LayoutRoot.BackgroundColor = colorNavy;
+
+            tipLabel.TextColor = colorSilver;
+       
+            totalLabel.TextColor = colorSilver;
+     
         }
 
     }
